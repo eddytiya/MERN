fe7fd47f121908d5ddb79da877f206fc4a29cc23
aditya pathak <102594671+eddytiya@users.noreply.github.com>
2026-06-16 16:58:45 +0530

    Add files via upload

---
+++ REACT/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FunctionController from './controllers/FunctionController'
+import ClassController from './controllers/ClassController'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Helllo</h2>
+      <FunctionController/>
+      <ClassController/>
+    </div>
+  )
+}
+
+export default App
---
+++ REACT/controllers/ClassController.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClassController extends Component {
+  render() {
+    return (
+      <div>
+        ClassController
+      </div>
+    )
+  }
+}
